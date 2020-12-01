bc5832cce1feffa174ee52c2f59a92c9557dd34f
unhurried <unhurried@users.noreply.github.com>
2020-12-01 22:23:01 +0900

    Modify configuration.

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const configuration = {
   clients: [{
     client_id: 'abc',
     client_secret: 'xyz',
+    scope: "openid offline_access s1 s2",
+    grant_types: ["authorization_code", "refresh_token"],
     redirect_uris: ['http://localhost/callback'],
-  }]
+  }],
+  scopes: ["openid", "offline_access", "s1", "s2"],
+  claims: {c1: null, c2: null},
+  acrValues: ["l1", "l2"],
+  features: {
+    claimsParameter: { enabled: true },
+  },
 };
 
 const oidc = new Provider('http://localhost:3000', configuration);
